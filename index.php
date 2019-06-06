d3a0ef63608101ec8bcb63eb938f557ef9a637ca
Mike Bywater <mike.j.bywater@gmail.com>
2019-06-06 14:27:18 +0100

    work on #1

--- index.php
+++ index.php
@@ -1,17 +1,31 @@
 <?php
 
-require 'vendor/autoload.php';
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $url  = parse_url($_SERVER['REQUEST_URI']);
+    $file = __DIR__ . $url['path'];
+    if (is_file($file)) {
+        return false;
+    }
+}
 
-function ServiceHandler() {
-    $data = new stdClass();
-    $data->Output = "Hello World!";
+require __DIR__ . '/../vendor/autoload.php';
 
-    return json_encode($data);
-};
+session_start();
 
-$app = new \Slim\App;
+// Instantiate the app
+$settings = require __DIR__ . '/../src/settings.php';
+$app = new \Slim\App($settings);
 
-$app->get('/', 'ServiceHandler');
-$app->post('/', 'ServiceHandler');
+// Set up dependencies
+require __DIR__ . '/../src/dependencies.php';
 
+// Register middleware
+require __DIR__ . '/../src/middleware.php';
+
+// Register routes
+require __DIR__ . '/../src/routes.php';
+
+// Run app
 $app->run();
